2235304f73b22cd3d7771326b1d743d59fcadf1f
Jesus Gastelum <JesusGastelum7@hotmail.com>
2020-09-18 23:43:36 -0600

    Docs agregados

---
+++ docs/precache-manifest.5f93a4fd77462c5920fbad39c5eb0823.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12417958dbdc7b1f1de48dada4866ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de89acf3d1c7b9ddac72",
+    "url": "/static/css/main.1041f713.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de89acf3d1c7b9ddac72",
+    "url": "/static/js/main.6b8caeed.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
